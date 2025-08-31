f49994e207154ab9a097964c5034d800191f9f47
Anushka Lall <anushkalall2011@gmail.com>
2025-08-30 21:22:39 -0400

    Add files via upload

---
+++ AC1.py
@@ -0,0 +1,10 @@
+
+def check_even_odd (number):
+   if number % 2 == 0:
+      number = str(number)
+      print(number + " is even")
+   elif number % 2 !3= 0:
+         number = str(number)
+         print(number + " is odd")
+x=int(input("Enter a Number:"))
+check_even_odd(x)
