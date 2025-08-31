a6497e8a45af2b43b58498222d49b21c0d5d55ea
Anushka Lall <anushkalall2011@gmail.com>
2025-08-31 10:13:27 -0400

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,30 @@
+#Variables
+
+# pets = "Dog" 
+# age = 10
+# dogAge= 12.3
+# isFemale = False
+
+# print(type(pets))
+# print(type(age))
+# print(type(dogAge))
+# print(type(isFemale))
+
+#Type Casting
+
+# num1 = 20
+# num2 = float(num1)
+# print(num1+num2)
+
+# num2 = 45.6
+# num1= int(num2)
+# print(num1+num2)
+
+# num1=str(40)
+# num2=str(num1)
+# print(num1+num2)
+
+#Words to not use as a variable name
+
+import keyword
+print(keyword.kwlist)
