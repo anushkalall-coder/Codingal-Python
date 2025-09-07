3f6bea7de329fa3b04fd6854ec728a2181eea324
Anushka Lall <anushkalall2011@gmail.com>
2025-09-06 21:03:52 -0400

    Add files via upload

---
+++ AC2.py
@@ -0,0 +1,30 @@
+name1 = ""
+name2 = ""
+name3 = ""
+name4 = ""
+name5 = ""
+
+dob1 = "mm/dd/y"
+dob2 = "mm/dd/y"
+dob3 = "mm/dd/y"
+dob4 = "mm/dd/y"
+dob5 = "mm/dd/y"
+
+
+name1= input("Enter Person 1 Name ")
+name2= input("Enter Person 2 Name ")
+name3= input("Enter Person 3 Name ")
+name4= input("Enter Person 4 Name ")
+name5= input("Enter Person 5 Name ")
+
+dob1 = input("Enter " + name1 + " Date of Birth" )
+dob2 = input("Enter " + name2 + " Date of Birth" )
+dob3 = input("Enter " + name3 + " Date of Birth" )
+dob4 = input("Enter " + name4 + " Date of Birth" )
+dob5 = input("Enter " + name5 + " Date of Birth" )
+
+print(name1 + " Date of Birth is " + dob1)
+print(name2 + " Date of Birth is " + dob2)
+print(name3 + " Date of Birth is " + dob3)
+print(name4 + " Date of Birth is " + dob4)
+print(name5 + " Date of Birth is " + dob5)
