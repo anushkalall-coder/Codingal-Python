7175e55677db03bb515afcbafda1cc6980a1e586
Anushka Lall <anushkalall2011@gmail.com>
2025-09-07 10:25:49 -0400

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,29 @@
+# name = "Anushka"
+# number1 = 20
+# number2 = float(number1)
+# isEven = False
+
+# print(number2)
+
+# Inputs:
+
+# DOB = input("When is your Birthday?: ")
+# print(DOB)
+
+# Pets = input("How many pets do you have?: ")
+# print(Pets)
+
+# Age = input("Are you 18?: ")
+# print(Age)
+
+# Functions:
+# Hi = "Hi There!"
+# print(Hi)
+
+# print(Hi.upper())
+# print(Hi.lower())
+# print(Hi.title())
+
+# print(Hi [0:2])
+# print(Hi[3:10])
+
