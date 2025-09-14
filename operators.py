d1f6191e27a6534000d44eca346053a14b09e08c
Anushka Lall <anushkalall2011@gmail.com>
2025-09-14 10:18:14 -0400

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,55 @@
+# Arthmetic Operators
+
+# a=3 
+# b=5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) decimal for division
+# print(a%b) remainder
+# print(a ** b)
+# print(a//b) no decimal for division
+
+# Comparison Operators
+
+# a=6
+# b=7
+
+# print(a==b) 
+# print(a!=b) not equal to
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# Assignment Opperators (when you can simplify the process)
+
+# a = 9
+
+# a+=3
+# print(a)
+# a-=4
+# print(a)
+# a*=5
+# print(a)
+# a/=6
+# a%=7
+# a //=8
+# a **= 9
+# print(a)
+
+# Storing Values
+# line1=67
+# line2=87
+# line3=94
+# line4=85
+# line5=11
+
+# #Finding the total of the lines
+# sum = line1+line2+line3+line4+line5
+# print("The sum of all lines is: " , sum)
+
+# #Find the average
+# average=sum/5
+# print("The average of all trees is: " , average)
