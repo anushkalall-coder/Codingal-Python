0c12e54c292a7bce1ad09d5967cc12ca94163fee
Anushka Lall <anushkalall2011@gmail.com>
2025-10-05 09:54:13 -0400

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,35 @@
+# name = "Anushka"
+
+# if name == "Anushka":
+#     print("Hey there")
+# else:
+#     print("What is your name?")
+
+# EligibleDrivingAge = 16
+
+# if EligibleDrivingAge >= 16:
+#     print("You are eligible to drive!")
+# else:
+#     print("You are not eligible to drive")
+
+# score = int(input("What percent did you get?"))
+# print(score) 
+
+# if score >= 90:
+#     print("You got an A! Great job!")
+# elif score>=80:
+#     print("You got a B! You are close to greatness!")
+# elif score>=70:
+#     print("You got a C. Keep working hard!")
+# elif score>=60:
+#     print("You got a D. Try harder.")
+# elif score <60:
+#     print("You failed.")
+
+# letter = input("Type a letter: ")
+# print(letter)
+
+# if letter=="A" or letter=="E" or letter== "I" or letter=="U" or letter=="O" or letter=="Y":
+#     print("This is a vowel")
+# else:
+#     print("This is a constant")
