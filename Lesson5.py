efab496d313c6920e75579a4c1ed8ca81c8ffe48
Anushka Lall <anushkalall2011@gmail.com>
2025-10-05 10:12:13 -0400

    Add files via upload

--- Lesson5.py
+++ Lesson5.py
@@ -33,3 +33,23 @@
 #     print("This is a vowel")
 # else:
 #     print("This is a constant")
+
+# height=float(input("Enter your height in cm: "))
+# weight=float(input("Enter your weight in kg: "))
+
+# BMI = weight/ (height/100)**2
+
+# print("Your BMI is",)
+
+# if BMI <= 18.4:
+#     print("You are underweight")
+# elif BMI <= 24.9:
+#     print("You are healthy")
+# elif BMI <= 29.9:
+#     print("You are overweight")
+# elif BMI <= 34.9:
+#     print("You are severly overweight")
+# elif BMI <= 39.9:
+#     print("You are obese")
+# else:
+#     print("You are severly obese")
