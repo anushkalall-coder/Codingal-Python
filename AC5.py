21089fcbeb750afbf2bb72c52fad95dffa5c8c45
Anushka Lall <anushkalall2011@gmail.com>
2025-10-17 20:15:00 -0400

    Add files via upload

---
+++ AC5.py
@@ -0,0 +1,7 @@
+character = input("Enter a character from the keyboard: ")
+
+if character == "a" or character == "b" or character == "c"or character == "d"or character == "e"or character == "f"or character == "g"or character == "h"or character == "i"or character == "j"or character == "k"or character == "l"or character == "m"or character == "n"or character == "o"or character == "p"or character == "q"or character == "r"or character == "s"or character == "t"or character == "u"or character == "v"or character == "w"or character == "x"or character == "y"or character == "z":
+    print("This character is part of the alphabet!")
+    print(character.lower())
+else:
+    print("This character is not part of the alphabet")
