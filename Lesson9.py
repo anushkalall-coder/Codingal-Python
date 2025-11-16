450969b1043b3f6c9bf70c7bf1a349fdfa3467e8
Anushka Lall <anushkalall2011@gmail.com>
2025-11-16 10:12:18 -0500

    Add files via upload

---
+++ Lesson9.py
@@ -0,0 +1,20 @@
+# number = int(input("Enter Number Here: "))
+
+# if number<0:
+#     print("Number is negative")
+# elif number>=0:
+#     print("Number is positive")
+# else:
+#     print("This is not a number")
+
+# n=int(input("Enter the number whose sum you want to find: "))
+# sum=0
+
+# for i in range(1, n+1):
+#     sum=sum+i
+#     print("Sum=", sum)
+
+number=0
+
+for i in range(1,11):
+    print(i)
