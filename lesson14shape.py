d0a8c2d7c45ea4a436f433709239647450ac93c9
Anushka Lall <anushkalall2011@gmail.com>
2026-01-19 13:11:13 -0500

    Add files via upload

---
+++ lesson14shape.py
@@ -0,0 +1,42 @@
+import turtle
+
+pen= turtle.Turtle()
+
+#square, rectangle, trapezoid, circle, triangle
+
+# pen.fd(100)
+# pen.right(90)
+# pen.fd(100)
+# pen.right(90)
+# pen.fd(100)
+# pen.right(90)
+# pen.fd(100)
+# pen.right(90)
+
+# pen.fd(100)
+# pen.right(90)
+# pen.fd(200)
+# pen.right(90)
+# pen.fd(100)
+# pen.right(90)
+# pen.fd(200)
+
+# pen.fd(200)
+# pen.right(60)
+# pen.fd(180)
+# pen.right(120)
+# pen.fd(380)
+# pen.right(120)
+# pen.fd(180)
+
+turtle.circle()
+
+# pen.fd(180)
+# pen.right(120)
+# pen.fd(180)
+# pen.right(120)
+# pen.fd(180)
+
+
+turtle.done
+
