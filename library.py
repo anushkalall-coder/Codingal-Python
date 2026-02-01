2043067d58e7a7f3dfc7a885730c9bca521dce3f
Anushka Lall <anushkalall2011@gmail.com>
2026-02-01 12:20:22 -0500

    Add files via upload

---
+++ library.py
@@ -0,0 +1,20 @@
+# import random
+# play=True
+# attempts=0
+# num=random.randint(10,20)
+
+# while play:
+#     guess=int(input("Guess the correct number between 10 and 20: "))
+#     attempts+=1
+#     if guess==num:
+#         print(f"Yay, you guessed the correct number in {attempts} attempts!")
+#         play=False
+#     else:
+#         print("Try Again")
+
+import math
+print(math.ceil(23.4))
+print(math.floor(23.4))
+print(math.lcm(4,8))
+print(math.gcd(8,16))
+print(math.factorial(60))
