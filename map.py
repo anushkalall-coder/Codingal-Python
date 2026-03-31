802afcf77f55b78f33c44817614b66c5ad5ff9de
Anushka Lall <anushkalall2011@gmail.com>
2026-03-31 11:59:57 -0400

    Add files via upload

---
+++ map.py
@@ -0,0 +1,38 @@
+numbers1=[1,2,3]
+numbers2=[4,5,6]
+result=map(lambda x, y: x+y, numbers1, numbers2)
+print("Addition of two lists")
+print(list(result))
+
+nums = [1,2,3,4,5]
+def sq(n):
+    return n*n
+square = list(map(sq, nums))
+print("Square of numbers in list")
+print(square)
+
+#Zip elements of two lists
+s1 = {2,3,1}
+s2 = {'b', 'a', 'c'}
+s3 = list(zip(s1,s2))
+print(s3,"\n")
+
+list1=[10,20,30,40]
+list2=[100,200,300,400]
+
+for x,y in zip(list1,list2[::-1]):
+    print(x,y)
+
+stocks=['reliance', 'infosys', 'tcs']
+prices=[2175,1127,2750]
+
+new_dict={stocks:prices for stocks, prices in zip(stocks, prices)}
+print('\n{}'.format(new_dict))
+
+#python program to demonstrate
+
+for i in range(10):
+    if i==5:
+        print(exit)
+        exit()
+    print(i)
