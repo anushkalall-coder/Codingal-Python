016ececfc342bdc0ced983b713cd1b8444fd6761
Anushka Lall <anushkalall2011@gmail.com>
2026-03-31 11:30:18 -0400

    Add files via upload

---
+++ ACset.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Print result
+print("Symmetric Difference:", result)
